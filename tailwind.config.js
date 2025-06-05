22c0ea7cb2d99f72c41c45b63c475ac81fdd686f
Abdel-Maxime <daon.maxime@gmail.com>
2025-06-05 13:31:49 +0200

    amélioration hero

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,18 @@ export default {
         primary: '#3da35d',
         secondary: '#ff6b35',
       },
+      backgroundSize: {
+        '200%': '200% 200%',
+      },
+      animation: {
+        'gradient-x': 'gradient-x 6s ease infinite',
+      },
+      keyframes: {
+        'gradient-x': {
+          '0%, 100%': {'background-position': '0% 50%'},
+          '50%': {'background-position': '100% 50%'},
+        },
+      },
     },
   },
   plugins: [],
